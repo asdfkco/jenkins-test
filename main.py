0f78c695ab47d0045c9b3b1d869ff9799f7e99ed
asdfkco <84617097+asdfkco@users.noreply.github.com>
2025-06-19 15:55:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI + Docker + Jenkins! + argocd asdf"}
+    return {"message": "Hello from FastAPI + Docker + Jenkins! + argocd image updater"}
